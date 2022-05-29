a3d38123e9768a6a8870aa9e486f09025b521f08
MikhailTseytlin <mikhailtseytlin@gmail.com>
2022-05-29 22:25:18 +0300

    1

---
+++ config.py
@@ -0,0 +1,4 @@
+token = '7ae6902347c6a352627bc5480e4632dc85035e5d4e29ede8ad7e9ee3d8234810b55bcc67f042d818c543b'
+key = 'b927659ad5a7f33d1c8d49755e122d9f77111fe1358029577fe4bd0e317bd58a5dc1a66203a81476bd13a'
+ya_token = 'AQAAAAAgINNLAAewYGs8s7MyxkMmq_M2_19cewU'
+#https://oauth.vk.com/authorize?client_id=7823527&display=page&scope=friends,groups,photos&response_type=token&v=5.92&state=123456
