eb495312abddf61ed0cfc20fba336e094430c186
MikhailTseytlin <mikhailtseytlin@gmail.com>
2022-05-30 21:56:07 +0300

    Bug fixing

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-token = '7ae6902347c6a352627bc5480e4632dc85035e5d4e29ede8ad7e9ee3d8234810b55bcc67f042d818c543b'
+token = str(os.getenv('token'))
 key = str(os.getenv('key'))
 ya_token = str(os.getenv('ya_token'))
 #https://oauth.vk.com/authorize?client_id=7823527&display=page&scope=friends,groups,photos&response_type=token&v=5.92&state=123456
